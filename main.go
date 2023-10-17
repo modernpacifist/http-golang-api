a4343e5079c907665239ca8dec0cd4c158351d13
modernpacifist <modernpacifist@gmail.com>
2023-10-17 11:55:02 +0300

    proelium-exercitum:17/10/2023-11:55:02 automated backup commit

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"net/http"
 
 	"http-golang-api/db"
-	""
+
+	"github.com/swaggo/http-swagger"
+	_ "github.com/swaggo/http-swagger/example/go-chi/docs"
 )
+
+type User struct {
+	ID int `json:"id"`
+	Name int `json:"name"`
+	Age int `json:"age"`
+	Salary int `json:"salary"`
+	Occupation int `json:"salary"`
+}
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
@@ -14,6 +24,10 @@ func helloHandler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	db.DbConnect()
+
+	http.Handle("/swagger/", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),
+	))
 
 	http.HandleFunc("/hello", helloHandler)
 
