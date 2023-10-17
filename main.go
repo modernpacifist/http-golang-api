4048b8c397a31efb9f85d01aee051c71e781fae9
modernpacifist <modernpacifist@gmail.com>
2023-10-17 13:55:01 +0300

    proelium-exercitum:17/10/2023-13:55:01 automated backup commit

--- main.go
+++ main.go
@@ -14,13 +14,31 @@ import (
 )
 
 func addUserHandler(w http.ResponseWriter, r *http.Request) {
-	user := types.User{
-		ID: 1, 
-		Name: "john",
-		Age: 21,
-		Salary: 100,
-		Occupation: "occupation1",
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed, http.StatusMethodNotAllowed")
+		return
 	}
+
+	var user types.User
+	err := json.NewDecoder(r.Boby).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("Received user", user)
+
+	response := struct {
+
+	}
+
+	//user := types.User{
+		//ID: 1, 
+		//Name: "john",
+		//Age: 21,
+		//Salary: 100,
+		//Occupation: "occupation1",
+	//}
 
 	jsonData, _ := json.Marshal(user)
 	fmt.Println(jsonData)
@@ -46,9 +64,8 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	//http.HandleFunc("/", helloHandler)
-	http.HandleFunc("/adduser/", addUserHandler)
-	http.HandleFunc("/getuser/", getUserHandler)
+	http.HandleFunc("/api/adduser/", addUserHandler)
+	http.HandleFunc("/api/getuser/", getUserHandler)
 
 	http.Handle("/swagger/", httpSwagger.Handler(
 		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),
